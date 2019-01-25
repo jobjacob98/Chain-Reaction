af095f09778734c248e14b0230fb7b713452a7e9
Job Jacob <jobjacob98@gmail.com>
2019-01-26 01:27:10 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,11 +3,9 @@ import json
 
 from agent.alpha_beta import *
 from agent.board import *
-from agent.DQN import *
 import time
 
 board = Board()
-network = QNetwork()
 
 app = Flask(__name__)
 app.config["DEBUG"] = False
@@ -22,34 +20,9 @@ def main():
 	agent_turn = int(request.form['player'])
 	cur_board = json.loads(request.form['board'], encoding="utf-8")
 
-	board.input(cur_board,agent_turn)
+	board.input(cur_board, agent_turn)
 
 	if game_mode == 2:
 		return json.dumps(list(alpha_beta(board,4)))
-
-	elif game_mode == 3:
-		if board.player == 1:
-			file_name = 'agent/player1.h5'
-		else:
-			file_name = 'agent/player2.h5'
-		with network.graph[0].as_default():
-			action = network.act(board,file_name = file_name)
-			action=[int(action[0]),int(action[1])]
-
-		return json.dumps(action)
-
-	elif game_mode == 4:
-		agent1_turn = request.form['agent1']
-		if (int(agent1_turn)==1 and int(agent_turn==1)) or (int(agent1_turn)==-1 and int(agent_turn)==-1):
-			return json.dumps(list(alpha_beta(board,4)))
-		elif (int(agent1_turn)==1 and int(agent_turn)==-1) or (int(agent1_turn)==-1 and int(agent_turn)==1):
-			if board.player == 1:
-				file_name = 'agent/player1.h5'
-			else:
-				file_name = 'agent/player2.h5'
-			with network.graph[0].as_default():
-				action = network.act(board,file_name = file_name)
-				action=[int(action[0]),int(action[1])]
-			return json.dumps(action)
 
 app.run()
